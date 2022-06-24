1601e5ba5a32125f835836191f156d006a1f5322
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-06-24 13:06:20 +0300

    initial cimmit

--- I4G034758JYJ/blog/views.py
+++ I4G034758JYJ/blog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
+from django.views import generic
+
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'templetes/post_list.html'
+
+
+class PostCreateView(generic.CreateView):
+    model: Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog: all")
+    template_name = 'templetes/post_form.html'
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'templetes/post_details.html'
+
+
+class PostUpdateView(generic.UpdateView):
+    model: Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog: all")
+    template_name = 'templetes/post_form.html'
+
+
+class PostDeleteView(generic.DeleteView):
+    model: Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog: all")
+    template_name = 'templetes/post_confirm_delete.html'
