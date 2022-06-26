8594939b44c5cc7cd80805a363d5989ec1b7378b
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-06-26 11:47:51 +0300

    init

--- I4G034758JYJ/blog/urls.py
+++ I4G034758JYJ/blog/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="all"),
     path("create/", views.PostCreateView.as_view(), name="post_create"),
     path("delete/<slug:slug>", views.PostDeleteView.as_view(),
-         name="post_confirm_delete"),
-    path("update/<slug:slug>", views.PostUpdateView.as_view(), name="post_form"),
+         name="post_delete"),
+    path("update/<slug:slug>", views.PostUpdateView.as_view(), name="post_update"),
     path("read/<slug:slug>", views.PostDetailView.as_view(), name="post_detail"),
 
 ]
